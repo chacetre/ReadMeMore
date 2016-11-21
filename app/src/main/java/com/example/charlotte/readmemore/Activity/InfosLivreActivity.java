2cbc3a49d42ab33af57515d8c9b62e9e47a6cdd3
Charlotte C <cha.cetre@gmail.com>
2016-11-21 10:49:20 -0500

    ajout des bouton back

--- app/src/main/java/com/example/charlotte/readmemore/Activity/InfosLivreActivity.java
+++ app/src/main/java/com/example/charlotte/readmemore/Activity/InfosLivreActivity.java
@@ -1,7 +1,10 @@
 package com.example.charlotte.readmemore.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.charlotte.readmemore.R;
 
@@ -10,9 +13,21 @@ import com.example.charlotte.readmemore.R;
  */
 public class InfosLivreActivity extends AppCompatActivity {
 
+    private ImageView backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infos_livre_activity);
+
+        backHome = (ImageView) findViewById(R.id.backHome) ;
+
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InfosLivreActivity.this, ListGeneralActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/charlotte/readmemore/Activity/SuggestionActivity.java
+++ app/src/main/java/com/example/charlotte/readmemore/Activity/SuggestionActivity.java
@@ -1,10 +1,13 @@
 package com.example.charlotte.readmemore.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.example.charlotte.readmemore.ListFragment.ListReadFragment;
@@ -23,10 +26,22 @@ import java.util.Vector;
  * Created by Charlotte on 23/10/2016.
  */
 public class SuggestionActivity extends AppCompatActivity {
+
+    private ImageView backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.suggestion_activity);
+        backHome = (ImageView) findViewById(R.id.backHome) ;
+
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SuggestionActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
 
         List fragments = new Vector();
 
